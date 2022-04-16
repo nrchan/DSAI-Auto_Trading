dc50914c81cba1ebd426d09396afeeaaf9935e54
nrchan <luka.iwt@gmail.com>
2022-04-16 10:34:12 +0800

    initial commit

---
+++ trader.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+if __name__ == "__main__":
+    # You should not modify this part.
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--training", default="training_data.csv", help="input training data file name")
+    parser.add_argument("--testing", default="testing_data.csv", help="input testing data file name")
+    parser.add_argument("--output", default="output.csv", help="output file name")
+    args = parser.parse_args()
+
+    # The following part is an example.
+    # You can modify it at will.
+    training_data = pd.read_csv(args.training, header = None)
+    print(training_data)
+
+    """
+    with open(args.output, "w") as output_file:
+        for row in testing_data:
+            # We will perform your action as the open price in the next day.
+            action = trader.predict_action(row)
+            output_file.write(action)
+
+            # this is your option, you can leave it empty.
+            trader.re_training()
+    """
