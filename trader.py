b59a0c7419b387e0da41a94a46864413505a9a72
nrchan <luka.iwt@gmail.com>
2022-04-17 20:22:37 +0800

    try to predict d+2

--- trader.py
+++ trader.py
@@ -11,13 +11,13 @@ EPOCH = 100
 PREDICT_DAYS = 19
 TESTING = True
 
-def make_data(data, window):
+def make_data(data, window, step=1, rshift=0):
     x = []
     y = []
-    for i in range(len(data) - window):
-        row = [r for r in data[i:i+window]]
+    for i in range(len(data) - step*window - rshift):
+        row = [r for r in data[i:i+step*window:step]]
         x.append(row)
-        y.append(data[i+window][0]) #take only opening price
+        y.append(data[i+step*window+rshift])
     return np.array(x), np.array(y)
 
 def take_input(data, window):
@@ -46,11 +46,11 @@ def predict_action(pred, CurrentStock, CurrentPrice):
 def make_model():
     model = Sequential()
     model.add(InputLayer((WINDOW, 4)))
-    model.add(LSTM(128))
+    model.add(LSTM(256))
+    model.add(Dense(128, activation="leaky_relu"))
     model.add(Dense(64, activation="leaky_relu"))
-    model.add(Dense(16, activation="leaky_relu"))
-    model.add(Dense(8, activation="leaky_relu"))
-    model.add(Dense(1))
+    model.add(Dense(32, activation="leaky_relu"))
+    model.add(Dense(4))
     return model
 
 if __name__ == "__main__":
@@ -82,36 +82,53 @@ if __name__ == "__main__":
     cb = tf.keras.callbacks.EarlyStopping(monitor='val_mse', patience=15, restore_best_weights=True)
 
     trader.fit(train_x, train_y, epochs = EPOCH, validation_split=0.2, callbacks=cb)
-"""
+
     testing_data = pd.read_csv(args.testing, header = None).to_numpy()
 
     count = 0
     prediction = []
+    prediction2 = []
     truth = []
 
     with open(args.output, "w") as output_file:
         for row in testing_data:
             if count == PREDICT_DAYS:
                 break
-            # We will perform your action as the open price in the next day.
+            # Extract data to predict from training data
             test_x = take_input(training_data, WINDOW)
-            pred = trader.predict(test_x).flatten().item()
+
+            # Predict
+            pred = trader.predict(test_x).flatten()
+
+            # Predict d+2
+            training_data = np.concatenate([training_data, [pred]], axis = 0)
+            test_x2 = take_input(training_data, WINDOW)
+            pred2 = trader.predict(test_x2).flatten()
+            training_data = training_data[:-1]
+
+            # Put new into train data
             training_data = np.concatenate([training_data, [row]], axis = 0)
-            action, CurrentStock = predict_action(pred, CurrentStock, CurrentPrice)
+
+            # Decide action and write
+            action, CurrentStock = predict_action(pred[0], CurrentStock, CurrentPrice)
             output_file.write("{}{}".format(action, "\n" if count < PREDICT_DAYS-1 else ""))
+
+            # Record
             CurrentPrice = row[0]
             prediction.append(pred)
+            prediction2.append(pred2)
             truth.append(row[0])
 
-            # this is your option, you can leave it empty.
             #trader.re_training()
 
             count += 1
 
     if TESTING:
-        plt.plot(prediction, label = "Predition")
+        opening = [p[0] for p in prediction]
+        opening2 = [p[0] for p in prediction2]
+        plt.plot(opening, label = "Predition")
+        plt.plot(list(range(1, PREDICT_DAYS)),opening2[:-1], label = "Predition")
         plt.plot(truth, label = "Truth")
         plt.legend(loc = "best")
         plt.show()
-        print(mean_squared_error(prediction, truth))
-"""
+        print(mean_squared_error(opening, truth))
